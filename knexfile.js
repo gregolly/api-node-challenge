14289756d2ea347f95fe91be6d026e9bdb53e1b0
gregolly <gregollyff@hotmail.com>
2023-07-10 13:26:33 -0400

    knex config

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, 'src', 'database', 'database.db')
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done)
+      }
+    },
+    migrations: {
+      directory: path.resolve(__dirname,'src', 'database', 'knex', 'migrations')
+    },
+    useNullAsDefault: true,
+  }
+}
