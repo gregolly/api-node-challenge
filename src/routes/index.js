3711d20690b5d399ba5f6b7eedee611ec7284bf2
gregolly <gregollyff@hotmail.com>
2023-07-11 13:26:28 -0400

    routes index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const { Router } = require('express')
 
 const usersRouter = require('./users.routes')
+const movieNotesRouter = require('./movie_notes.routes')
+const movieTagsRouter = require('./movie_tags.routes')
 
 const routes = Router()
 
 routes.use("/users", usersRouter)
+routes.use("/movieNotes", movieNotesRouter)
+routes.use("/movieTags", movieTagsRouter)
 
 module.exports = routes
