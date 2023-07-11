9282d7f5b4a00e70bf9ed16c5fc7fa9dc24be566
gregolly <gregollyff@hotmail.com>
2023-07-11 13:25:07 -0400

    Movie tags controller

---
+++ src/controllers/MovieTagsController.js
@@ -0,0 +1,15 @@
+const knex = require("../database/knex")
+
+class MovieTagsController {
+    async index(req, res) {
+        const { user_id } = req.params
+
+        const tags = await knex('movie_tags')
+        .where({ user_id })
+
+
+        return res.json(tags)
+    }
+}
+
+module.exports = MovieTagsController
